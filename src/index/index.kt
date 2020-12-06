69cab8ee61a9d5a0cafaf585c15dfcb28be55476
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2020-12-06 23:46:55 +0900

    初期化

--- src/index/index.kt
+++ src/index/index.kt
@@ -1,13 +1,10 @@
 package index
 
 import app.*
-import kotlinext.js.*
 import react.dom.*
-import kotlin.browser.*
+import kotlinx.browser.*
 
 fun main(args: Array<String>) {
-    requireAll(require.context("src", true, js("/\\.css$/")))
-
     render(document.getElementById("root")) {
         app()
     }
